96e3a8d4445eadef29c2e2068d2aee4e2a077a60
JonasNation <63250967+JonasNation@users.noreply.github.com>
2021-01-31 14:29:05 -0600

    Update ConnectionHelper.java

--- ConnectionHelper.java
+++ ConnectionHelper.java
@@ -6,7 +6,7 @@ import android.util.Log;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+// This connection helper is what makes the connection to your database
 public class ConnectionHelper {
     Connection con;
     String username, password, ip, port, database;
@@ -16,16 +16,17 @@ public class ConnectionHelper {
         ip = "recipeserver.database.windows.net";
         port = "1433";
         database = "recipeDB";
-        username = "jonasNation@recipeserver";
+        username = "jonasNation@recipeserver"; 
         password = "****************";
-
+        // StrictMode is responsible for the connection
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection connection = null;
         String ConnectionURL = null;
-
+                
         try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //you can find your connection string in your azure sql database overview
             ConnectionURL = "jdbc:jtds:sqlserver://"+ip+":"+port+";"+"databasename="+database+";user="+username+";password="+password+";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
             connection = DriverManager.getConnection(ConnectionURL);
         }catch (Exception ex) {
